934d584afd1654134c6afcd4b32a82fe04e1a67d
apookpun <44304255+apookpun@users.noreply.github.com>
2018-11-16 11:26:47 -0800

    changed repo and readme file

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Project 2
 
 1. create project2 folder in your user folder
-2. run ./project2.sh {USER}
+2. scp shell file to your project2 folder in your user
+3. run ./project2.sh {USER}
--- project2.sh
+++ project2.sh
@@ -1,14 +1,12 @@
-USER=$1
-RESOURCE=/projects/micb405/resources/project_2/2018
-PROJECT2=/home/$USER/project2
-
-cd $PROJECT2
-mkdir prokka_output
-
+USER=$1;
+RESOURCE=/projects/micb405/resources/project_2/2018;
+PROJECT2=/home/$USER/project2;
+cd $PROJECT2;
+mkdir prokka_output;
 for f in $RESOURCE/SaanichInlet_10m/MetaBAT2_SaanichInlet_10m/MedQPlus_MAGs/*fa;
     do sid=$( basename $f | sed 's/.fa//g' );
-    echo $sid; 
+    echo $sid;
     tax=$(grep -w $sid $RESOURCE/SaanichInlet_10m/MetaBAT2_SaanichInlet_10m/gtdbtk_output/gtdbtk.*.classification_pplacer.tsv | awk '{ print $2 }' | awk -F";" '{ print $1 }' | sed 's/d__//g');
     echo $sid,$tax;
-    prokka --kingdom $tax --outdir project2/prokka_output --force $f 
+    prokka --kingdom $tax --outdir $PROJECT2/prokka_output --force $f;
 done
