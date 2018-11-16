f2ada1f90fb2ca457ddb1fba202523d60801a75a
Zhang <u7x9a@ugrad.cs.ubc.ca>
2018-11-16 10:51:48 -0800

    project 2 shel

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# project2
+# Project 2
+
+1. create project2 folder in your user folder
+2. run ./project2.sh {USER}
---
+++ project2.sh
@@ -0,0 +1,14 @@
+USER=$1
+RESOURCE=/projects/micb405/resources/project_2/2018
+PROJECT2=/home/$USER/project2
+
+cd $PROJECT2
+mkdir prokka_output
+
+for f in $RESOURCE/SaanichInlet_10m/MetaBAT2_SaanichInlet_10m/MedQPlus_MAGs/*fa;
+    do sid=$( basename $f | sed 's/.fa//g' );
+    echo $sid; 
+    tax=$(grep -w $sid $RESOURCE/SaanichInlet_10m/MetaBAT2_SaanichInlet_10m/gtdbtk_output/gtdbtk.*.classification_pplacer.tsv | awk '{ print $2 }' | awk -F";" '{ print $1 }' | sed 's/d__//g');
+    echo $sid,$tax;
+    prokka --kingdom $tax --outdir project2/prokka_output --force $f 
+done
