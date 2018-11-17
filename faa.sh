5b1bb4ec5a046258bcad8ef1dc06a615040619ed
Zhang <u7x9a@ugrad.cs.ubc.ca>
2018-11-17 10:58:38 -0800

    kaas stuff

--- faa.sh
+++ faa.sh
@@ -1,15 +1,20 @@
 USER=$1
 RESOURCE=/projects/micb405/resources/project_2/2018
 PROJECT2=/home/$USER/project2
-cd $PROJECT2
-mkdir prokka_output
 PROKKA_OUTPUT=/home/$USER/project2/prokka_output
+KAAS=$PROJECT2/kaas
+cd $PROJECT2
+if [ ! -d $KAAS ]; then
+    mkdir kaas
+fi
 
-for f in $PROJECT2/*; do
-    folder=$( basename $f | sed 's///g' );
-    echo $folder;
-    for f in $PROJECT2/$folder/*.faa; do
+for f in $PROKKA_OUTPUT/*; do
+    folder=$f;
+    # echo $folder;
+    for f in $folder/*.faa; do
         prefix=$( basename $f | sed 's/.faa//g' );
-        echo $prefix;
+        echo 'concatenating $folder/$prefix';
+        cat $folder/$prefix.faa >$KAAS/fatfile.fasta;
+        echo 'done';
     done
 done
