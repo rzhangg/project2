9d2703308c50309398ba2e455be50f5672722f5b
apookpun <44304255+apookpun@users.noreply.github.com>
2018-11-20 12:18:26 -0800

    added rpkm shell

---
+++ rpkm.sh
@@ -0,0 +1,16 @@
+USER=$1
+RESOURCE=/projects/micb405/resources/project_2/2018
+PROJECT2=/home/$USER/project2
+cd $PROJECT2
+SAM=$PROJECT2/sam
+RPKM=$PROJECT2/rpkm_output
+BWA=$PROJECT2/bwa
+if [ ! -d $SAM ]; then
+    mkdir rpkm_output
+fi
+for f in $SAM/*.sam; do
+
+    prefix=$( basename $f | sed 's/.sam//g');
+    echo $prefix;
+    $RESOURCE/rpkm -c $BWA/fat_reference.fasta -a $SAM/$prefix.sam -o $RPKM/$prefix.rpkm.csv
+done
